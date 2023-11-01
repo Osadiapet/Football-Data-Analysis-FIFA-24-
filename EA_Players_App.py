c5fd860fcb217330e0bce58e12ef2bd49af6e2fb
osadiapet <osadiepet@gmail.com>
2023-11-01 22:47:24 +0000

    added picture

--- EA_Players_App.py
+++ EA_Players_App.py
@@ -21,7 +21,7 @@ players_data = pd.read_csv('male_players.csv')
 selected_columns = [
     'long_name', 'update_as_of', 'value_eur', 'wage_eur', 'age', 'dob', 'height_cm',
     'weight_kg', 'overall', 'potential', 'skill_moves', 'pace', 'shooting',
-    'passing', 'dribbling', 'defending', 'physic', 'attacking_crossing',
+    'passing', 'dribbling', 'defending', 'physic', 'attacking_crossing', 'player_url',
     'attacking_finishing', 'attacking_heading_accuracy', 'preferred_foot',
     'work_rate', 'body_type', 'release_clause_eur', 'player_positions',
     'attacking_short_passing', 'attacking_volleys', 'skill_dribbling',
@@ -39,14 +39,15 @@ selected_columns = [
 players_data = players_data[selected_columns]
 
 # Main page title and description
-st.title("EA Sports '24 Player Analysis")
+st.title("FIFA'24 Player Analysis (2014-2024)")
 st.write(
     "Welcome to the Football Data Analysis app. Explore various aspects of football player data, including attributes, player rankings, and more."
 )
 
 
+
 # Data Wrangling section
-st.header("Data Wrangling")
+st.header("Data overview")
 st.write("Let's start by exploring the data and its attributes.")
 
 # Show first rows of the data
@@ -54,15 +55,39 @@ if st.checkbox("Show Data"):
     st.dataframe(players_data.head())
 
 # Number of players in the dataset
-if st.checkbox('Show Number of players'):
+if st.checkbox('Show Number of Players'):
     num_players = players_data['long_name'].nunique()
     st.write(f"Number of players in the dataset: {num_players}")
 
 # Descriptive statistics
 st.subheader("Descriptive Statistics")
-st.write("Descriptive statistics of player attributes.")
-if st.checkbox("Show Statistics"):
+st.write("Descriptive Statistics of Player Attributes.")
+if st.checkbox("Show Descriptive Statistics"):
     st.write(players_data.describe())
+
+
+# Find the oldest player in the dataset
+oldest_player = players_data.loc[players_data['age'] == players_data['age'].max()]
+
+if st.checkbox('Show Oldest Player'):
+    st.write("Oldest Player Details:")
+    st.write(f"Name: {oldest_player['long_name'].values[0]}")
+    st.write(f"Club: {oldest_player['club_name'].values[0]}")
+    st.write(f"Country: {oldest_player['nationality_name'].values[0]}")
+    st.write(f"Age: {oldest_player['age'].values[0]}")
+    st.write(f"Overall Rating: {oldest_player['overall'].values[0]}")
+
+
+# Find the oldest player in the dataset
+youngest_player = players_data.loc[players_data['age'] == players_data['age'].min()]
+
+if st.checkbox('Show Youngest Player'):
+    st.write("Youngest Player Details:")
+    st.write(f"Name: {youngest_player['long_name'].values[0]}")
+    st.write(f"Club: {youngest_player['club_name'].values[0]}")
+    st.write(f"Country: {youngest_player['nationality_name'].values[0]}")
+    st.write(f"Age: {youngest_player['age'].values[0]}")
+    st.write(f"Overall Rating: {youngest_player['overall'].values[0]}")
 
 
 # Define a function to categorize player positions
@@ -82,7 +107,7 @@ def categorize_positions(positions):
 players_data['position_category'] = players_data['player_positions'].apply(categorize_positions)
 
 # Add a checkbox to show the pie chart
-show_pie_chart = st.checkbox("Show Pie Chart")
+show_pie_chart = st.checkbox("Show Pie Chart of Player Positions")
 
 if show_pie_chart:
     # Count the number of players in each category
@@ -98,12 +123,12 @@ if show_pie_chart:
     # Display the pie chart
     st.plotly_chart(fig)
 else:
-    st.write("Check the box above to show the pie chart.")
+    st.write("Check the box above to show distribution of player positions.")
 
 
 
 # Add a checkbox to show the pie chart
-show_pie_chart = st.checkbox("Show Pie Chart of Players Preferred Foot")
+show_pie_chart = st.checkbox("Show Pie Chart of Distribution of Players Preferred Foot")
 
 if show_pie_chart:
     # Count the number of players in each category
@@ -123,7 +148,7 @@ else:
 
 
 # Create a checkbox to display the trend
-show_trend = st.checkbox('Show Trend of Average Overall Rating')
+show_trend = st.checkbox('Show Trend of Average Overall Rating Overtime')
 
 if show_trend:
     # Convert the 'date' column to datetime format
@@ -144,8 +169,7 @@ if show_trend:
     ax.set_title('Trend of Average Overall Rating')
     
     # Set the y-axis limit to start from 0 and end at 100
-    ax.set_ylim(0, 100)
-
+    ax.set_ylim(40, 100)
     ax.grid(True)
 
     # Add data labels to the data points
@@ -156,7 +180,7 @@ if show_trend:
 
 
 # Create a multiselect dropdown to select a team
-selected_teams = st.multiselect('Select a Team', players_data['club_name'].unique())
+selected_teams = st.multiselect('Select a Team to Show the Trend of Average Rating Overtime', players_data['club_name'].unique())
 
 if selected_teams:
     selected_team = selected_teams[0]  # Get the first (and presumably only) selected team
@@ -179,7 +203,7 @@ if selected_teams:
     ax.plot(average_ratings_by_year.index, average_ratings_by_year.values, marker='o', linestyle='-')
     
     # Set the y-axis limit to start from 0
-    ax.set_ylim(0, 100)
+    ax.set_ylim(40, 100)
 
     ax.set_xlabel('Year')
     ax.set_ylabel('Average Overall Rating')
@@ -193,14 +217,15 @@ if selected_teams:
     st.pyplot(fig)
 
 
+
 # Create a multiselect dropdown to select a player
-selected_players = st.sidebar.multiselect('Select a Player', players_data['long_name'].unique())
+selected_players = st.sidebar.multiselect('Select Player', players_data['long_name'].unique())
 
 if selected_players:
-    # Retrieve the information for the selected teams
-    selected_player_info = players_data[players_data['long_name'].isin(selected_players)]
+    # Retrieve the information for the selected player
+    selected_player_info = players_data[players_data['long_name'] == selected_players[0]]  
 
-    # Display the selected teams' information in a tabular form
+    # Display the selected player's information in a tabular form
     st.subheader("Information of Selected Player")
     st.dataframe(selected_player_info.T)
 
@@ -210,7 +235,7 @@ if selected_players:
     st.dataframe(player_numerical_stats.T)
 
 else:
-    st.warning("Please select one or more players from the dropdown to view their information and statistics.")
+    st.warning("Please select a player from the dropdown to view their information and statistics.")
 
 
 # Create a multiselect dropdown to select a player
@@ -250,8 +275,8 @@ if selected_players:
     st.pyplot(fig)
 
 
-# Create a multiselect widget to choose one or more player
-selected_teams = st.sidebar.multiselect("Select Teams", players_data['club_name'])
+# Create a multiselect widget to choose one or more team
+selected_teams = st.sidebar.multiselect("Select Team", players_data['club_name'])
 
 if selected_teams:
     # Retrieve the information for the selected teams
@@ -267,11 +292,11 @@ if selected_teams:
     st.dataframe(team_numerical_stats.T)
 
 else:
-    st.warning("Please select one or more teams from the dropdown to view their information and statistics.")
+    st.warning("Please select a team from the dropdown to view their information and statistics.")
 
 
 # Create a multiselect widget to choose one or more countries
-selected_countries = st.sidebar.multiselect("Select Countries", players_data['nationality_name'])
+selected_countries = st.sidebar.multiselect("Select Country", players_data['nationality_name'])
 
 if selected_countries:
     # Retrieve the information for the selected countries
@@ -283,24 +308,20 @@ if selected_countries:
 
     # Calculate and display descriptive statistics for numerical variables
     country_numerical_stats = selected_country_info.describe()
-    st.subheader("Descriptive Statistics for Selected Countries")
+    st.subheader("Descriptive Statistics for Selected Country")
     st.dataframe(country_numerical_stats.T)
 else:
-    st.warning("Please select one or more countries from the dropdown to view their information and statistics.")
+    st.warning("Please select a country from the dropdown to view their information and statistics.")
 
 
 # Data Visualization section
 st.header("Data Visualization")
 st.write("Explore the distribution of player attributes and their relationships.")
 
-# # Select attributes to plot
-# players_variables_to_plot = st.multiselect(
-#     "Select attributes to plot",
-#     selected_columns[2:],  # Exclude 'long_name'
-# )
+
 
 # Select attributes to plot
-selected_variable = st.multiselect("Select a variable", players_data.columns[2:])
+selected_variable = st.multiselect("Select a player attribute to show distribution", players_data.columns[2:])
 
 # Check if any attribute has been selected
 if selected_variable:
@@ -322,14 +343,14 @@ corr_columns = players_data[['overall', 'potential', 'pace', 'shooting',
     'attacking_finishing', 'attacking_heading_accuracy', 'skill_dribbling',
     'skill_fk_accuracy', 'skill_long_passing',
     'skill_ball_control', 'movement_acceleration', 'movement_sprint_speed',
-    'movement_agility', 'movement_reactions', 'movement_balance',
-    'power_shot_power', 'power_jumping', 'power_stamina', 'power_strength',
-    'power_long_shots', 'mentality_aggression',
+    'movement_agility',  'movement_balance',
+    'power_shot_power', 'power_jumping', 'power_stamina', 
+    'power_long_shots', 
     'mentality_positioning', 'mentality_vision', 'mentality_penalties',
     'mentality_composure', 'defending_marking_awareness', 
     'defending_standing_tackle', 'defending_sliding_tackle']]
 
-st.subheader("Correlation Heatmap")
+st.subheader("Show Correlation Heatmap of Player Attributes")
 st.write("Explore the relationships between key player attributes.")
 if st.checkbox("Show Correlation Heatmap"):
     # Create a heatmap of attribute correlations
@@ -339,8 +360,10 @@ if st.checkbox("Show Correlation Heatmap"):
     st.pyplot()
 
 # Top Players by Average Overall Rating section
-st.header("Top Players by Average Overall Rating Over Time")
-st.write("Visualize the top players by their average overall rating for the past 9 years.")
+# Using HTML to style the header text
+# st.write("<h2>Top 10 Teams by Average Overall Rating of Players</h2>", unsafe_allow_html=True)
+st.subheader("Top Players by Average Overall Rating Over Time")
+st.write("Visualize the top players by their average overall rating")
 
 # Group by player_name and calculate the average overall rating
 top_10_players = players_data.groupby('long_name')[['overall']].mean().sort_values(by='overall', ascending=False).head(10)
@@ -350,7 +373,8 @@ if st.checkbox("Show Top 10 Players"):
     plt.figure(figsize=(12, 6))
     bars = plt.barh(top_10_players.index, top_10_players['overall'], color='skyblue')
     plt.xlabel('Average Overall Rating')
-    plt.title('Top 10 Players by Average Overall Rating Over Time')
+    
+    st.subheader('Top 10 Players by Average Overall Rating Over Time')
     plt.gca().invert_yaxis()  # Invert the y-axis for better visualization
 
     # Add data labels to the bars
@@ -362,8 +386,8 @@ if st.checkbox("Show Top 10 Players"):
 
 
 # Top tems by Average Overall Rating section
-st.header("Top Teams by Average Overall Rating of Players (2014 - 2023)")
-st.write("Visualize the top Teams by their Players Average Overall Rating for the Past 9 years.")
+st.subheader("Top 10 Teams by Average Overall Rating of Players")
+st.write("Visualize the top 10 Teams by their Players Average Overall Rating")
 
 # Group by player_name and calculate the average overall rating
 top_10_teams = players_data.groupby('club_name')[['overall']].mean().sort_values(by='overall', ascending=False).head(10)
@@ -384,7 +408,7 @@ if st.checkbox("Show Top 10 Teams"):
     st.pyplot()
 
 # Create a multiselect dropdown to select one or more categories
-selected_categories = st.multiselect('Select Category', players_data['position_category'].unique())
+selected_categories = st.multiselect('Select Position to View Top 10 Players', players_data['position_category'].unique())
 
 if selected_categories:
     # Filter data based on the selected category
